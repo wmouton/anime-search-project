b3bb927ac6fdee93e9939abbf3e76415416261c2
l33th <l33th@protonmail.ch>
2021-10-14 08:19:49 +0200

    Header Styling Grey

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/css/main.css'
+import './assets/scss/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
