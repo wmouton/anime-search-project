89221c83b57187b6dbb81e3510c1a462206747ee
l33th <l33th@protonmail.ch>
2021-10-14 07:50:45 +0200

    Anime Project Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> Anime Search </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
