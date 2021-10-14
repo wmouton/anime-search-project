1b1bf50c8c1419284795efffa0c78469ae0aeb74
l33th <l33th@protonmail.ch>
2021-10-14 09:02:48 +0200

    H3 Top Anime

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Sidebar = () => {
-    return (
-        <>
-            <aside>
-                <nav>
-                    <h3>
-                        <a href="#" target='_blank' rel='noreferrer'>
-                            Dragon Ball Super
-                        </a>
-                    </h3>
-                </nav>
-            </aside>
-        </>
-    )
-}
+	return (
+		<>
+			<aside>
+				<nav>
+                <h3>Top Anime</h3>
+					<a href='#' target='_blank' rel='noreferrer'>
+						Dragon Ball Super
+					</a>
+				</nav>
+			</aside>
+		</>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
