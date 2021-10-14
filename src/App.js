cca48723c5b576096f04b8d9b4a94e74ed280169
l33th <l33th@protonmail.ch>
2021-10-14 08:35:07 +0200

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 	const [animeList, setAnimeList] = useState([]);
@@ -9,6 +10,9 @@ function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<div className='content-wrap'>
+				<Sidebar topAnime={topAnime} />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <>
+            <aside>
+                <nav>
+                    <h3>
+                        <a href="#" target='_blank' rel='noreferrer'>
+                            Dragon Ball Super
+                        </a>
+                    </h3>
+                </nav>
+            </aside>
+        </>
+    )
+}
+
+export default Sidebar
