2d5c6d5154b875ef25dff8f273458924d0bb9584
l33th <l33th@protonmail.ch>
2021-10-14 08:30:11 +0200

    State Variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-    </div>
-  );
+	const [animeList, setAnimeList] = useState([]);
+	const [topAnime, setTopAnime] = useState([]);
+	const [search, setSearch] = useState("");
+
+	return (
+		<div className='App'>
+			<Header />
+		</div>
+	);
 }
 
 export default App;
