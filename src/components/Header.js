89e89fd52f202c9964c879c4c6de35a0b2cc4a10
l33th <l33th@protonmail.ch>
2021-10-14 07:58:19 +0200

    Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<header>
+				<h1>
+					The <strong>Anime Search</strong> Database
+				</h1>
+			</header>
+		</div>
+	);
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './assets/css/main.scss'
+import './assets/css/main.css'
 
 ReactDOM.render(
   <React.StrictMode>
