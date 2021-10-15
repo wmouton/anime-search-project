caa4e17d4ff18f882d4b2e42faacc2e630f506da
l33th <l33th@protonmail.ch>
2021-10-15 06:44:24 +0200

    Display Search

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const AnimeCard = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const AnimeCard = ({ anime }) => {
+	return (
+		<article className='anime-card'>
+			<a href={anime.url} target='_black' rel='noreferrer'>
+				<figure>
+					<img src={anime.image_url} alt='' />
+				</figure>
+				<h3>{anime.title}</h3>
+			</a>
+		</article>
+	);
+};
 
-export default AnimeCard
+export default AnimeCard;
